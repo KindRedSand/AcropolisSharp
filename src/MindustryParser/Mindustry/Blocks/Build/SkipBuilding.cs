ab3dcc631a5f7778855f83313863fb47a1c05999
KindRedSand <sand25g@gmail.com>
2025-04-05 14:45:32 +0300

    Idk why it get ignored by git

---
+++ src/MindustryParser/Mindustry/Blocks/Build/SkipBuilding.cs
@@ -0,0 +1,11 @@
+﻿using JavaStream;
+
+namespace Playground.Mindustry.Blocks.Build;
+
+public class SkipBuilding : Building
+{
+    protected override void read(DataInput read, sbyte revision, int len)
+    {
+        read.Skip(len);
+    }
+}
